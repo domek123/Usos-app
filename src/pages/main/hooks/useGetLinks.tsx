5c6e9d4c864a9b67c6eca07510671b015ad86666
Dominik Wójcik <dominikw883@gmail.com>
2025-12-04 16:04:44 +0100

    fix links

--- src/pages/main/hooks/useGetLinks.tsx
+++ src/pages/main/hooks/useGetLinks.tsx
@@ -1,29 +1,44 @@
-import { useFacultyStore } from "@/stores";
+import { useFacultyStore, useUserStore } from "@/stores";
+import { PermissionType } from "@/types";
 import { useTranslation } from "react-i18next";
 
 export const useGetLinks = () => {
   const { t } = useTranslation();
   const { faculty } = useFacultyStore();
+  const { role } = useUserStore();
 
   const studentLinks = [
     { to: "schedule", text: t("navigation.schedule") },
     { to: "grades", text: t("navigation.grades") },
-    { to: "contact", text: t("navigation.contact") },
   ];
 
   const adminLinks = [
     { to: "subjects", text: t("navigation.subjects") },
     { to: "students", text: t("navigation.students") },
-    { to: "contact", text: t("navigation.contact") },
   ];
 
   const adminGlobalLinks = [
-    { to: "/", text: "Dashboard" },
     { to: "teachers", text: t("navigation.teachers") },
     { to: "students", text: t("navigation.students") },
-    { to: "contact", text: t("navigation.contact") },
   ];
 
-  return { links: studentLinks };
-  return { links: faculty !== null ? adminLinks : adminGlobalLinks };
+  let links = [];
+
+  if (faculty === null) {
+    links.push({ to: "/", text: "Dashboard" });
+  }
+
+  if (role === PermissionType.STUDENT && faculty !== null) {
+    links = [...links, ...studentLinks];
+  } else if (role === PermissionType.ADMIN) {
+    if (faculty !== null) {
+      links = [...links, ...adminLinks];
+    } else {
+      links = [...links, ...adminGlobalLinks];
+    }
+  }
+
+  links = [...links, { to: "contact", text: t("navigation.contact") }];
+
+  return { links };
 };
